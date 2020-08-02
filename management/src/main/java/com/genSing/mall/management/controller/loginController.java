1c9ab579afe0d432041b217ba5fb63ce378fb7d0
Evilcrown <l>
2020-08-02 12:38:26 +0800

    集成spring security

--- management/src/main/java/com/genSing/mall/management/controller/loginController.java
+++ management/src/main/java/com/genSing/mall/management/controller/loginController.java
@@ -3,15 +3,17 @@ package com.genSing.mall.management.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 
 @RestController
+@RequestMapping("/user")
 @Api(value = "/user",tags = "用户接口")
 public class loginController {
 
-    @GetMapping("login")
+    @GetMapping("/hello")
     @ApiOperation("用户登录验证")
     public Object login(){
         HashMap<Object, Object> map = new HashMap<>();
